d652b34834bf226f88773813832913b39c2659a4
Tsunoyaka <mousakun@gmail.com>
2022-12-31 02:52:10 +0600

    normal_verse3

--- music.py
+++ music.py
@@ -164,8 +164,8 @@ def del_funk(commands):
 
 def music_btn(commands):
     if commands.data == 'mus_back':
-        del_funk(commands)
         albums(commands.message)
+        del_funk(commands)
     elif commands.data == 'music':
         albums(commands.message)
     elif commands.data == 'Fabula_Song':
